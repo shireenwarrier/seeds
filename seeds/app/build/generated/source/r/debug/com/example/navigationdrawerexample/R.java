6c8b5389dc4f1be1f507fa67242486aab23bd8f9
Anne Z <amzepecki@gmail.com>
2015-07-14 07:50:05 -0700

    updated launch icon

--- seeds/app/build/generated/source/r/debug/com/example/navigationdrawerexample/R.java
+++ seeds/app/build/generated/source/r/debug/com/example/navigationdrawerexample/R.java
@@ -137,8 +137,9 @@ public final class R {
     public static final class mipmap {
         public static final int avatar=0x7f030000;
         public static final int ic_l=0x7f030001;
-        public static final int splash=0x7f030002;
-        public static final int splash3=0x7f030003;
+        public static final int ic_launcher=0x7f030002;
+        public static final int splash=0x7f030003;
+        public static final int splash3=0x7f030004;
     }
     public static final class string {
         public static final int action_settings=0x7f090000;
